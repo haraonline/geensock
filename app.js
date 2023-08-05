c3649cc54f374f099ebe235af1ec0f17076c5cc8
Hara Gopal Kovvali <haragopal.kovvali@gmail.com>
2023-08-05 17:39:04 +0200

    reorganize project

--- app.js
+++ app.js
@@ -1,27 +1,29 @@
 
+// Context: This project using the GreenSock library to create animations for learning purposes
+// CDN Link to GSAP: https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/gsap.min.js
 
-//gsap.to(), gsap.from(), gsap.fromTo()
+// LESSON 1: BASIC TWEEN
+// simplest tween, moving the h3 element 200px to the right
+// the first argument is the target element
+// the code in the curly braces is called vars object using to animate the target element
+// gsap.to("h3", {x:200});
 
+// BASIC TWEEN WITH DURATION, ROTATION, EASE
+// gsap.to("img", { x:200, duration:3});
+// gsap.to(".fred1", { x:200, y:200, duration:3 });
+// gsap.to(".fred2", { x:200, y:200, duration:3, rotation:360 });
+// gsap.to(".fred1", { x:200, y:200, duration:3, rotation:360, ease:"bounce" });
 
-//gsap.to(".fred1", {duration: 3, x: 400});
-//gsap.fromTo(".fred1", {x:700, y:400, opacity:0}, {x: 400, y:200, duration: 3, scale:3, opacity:1});
+// GSAP can animation any numeric property of any object (not just DOM elements)
+// For example, width, height, opacity, border radius, color / background color, viewport heit, width etc.
 
-//special Properties: delay, repeat, yoyo, repeatDelay
-//gsap.to(".fred1", {duration: 3, x: 600, repeat:2, yoyo: true}); //repeat -1 for infinite loop
-//gsap.to(".fred2", {duration: 3, x: 600, delay: 2, });
+// LESSON 2: from() and fromTo() methods
 
-//gsap.to(".fred1", {duration: 3, x: 600}); // default: easeout
-//gsap.to(".fred1", {duration: 3, x: 600, ease: "bounce"});     //ease: bounce on the way out
-//gsap.to(".fred1", {duration: 3, x: 600, ease: "bounce.in"});  //ease: bounce on the way in
-//gsap.to(".fred1", {duration: 3, x: 600, ease: "bounce.inOut"});  //ease: bounce on the way InOut
-//gsap.to(".fred2", {duration: 3, x: 600, ease: "linear"}); //ease: linear. no ease
+// from() method - moves from 200px (starting position ) to the original position
+ //gsap.from("img", { x:200, y:200, duration:3});
 
-gsap.to(".fred1", {duration: 3, x: 600, ease: "bounce"});
-gsap.to(".fred2", {duration: 3, x: 600, ease: "back(6)"});
+// fromTo() method - moves from x:400, y:200, scale:3 to the position x:0, y:0, scale:1. We need two var objects here
+// gsap.fromTo("img", { x:400, y:200, scale:3, opacity:0 }, { x:50, y:50, scale:1, duration:3, opacity:3 });
 
-
-//ease options
-//https://greensock.com/docs/v3/Eases
-//linear, in, out, inout, back, elastic, bounce
-
-
+// LESSON 3: Special Properties: Delay and Repeat
+// TO BE CONTINUED ...   
