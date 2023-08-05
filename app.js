416cd31b8233271ae3d6d96066c2cd0dee231a37
Hara Gopal Kovvali <haragopal.kovvali@gmail.com>
2023-08-05 20:37:56 +0200

    ease

--- app.js
+++ app.js
@@ -25,5 +25,19 @@
 // fromTo() method - moves from x:400, y:200, scale:3 to the position x:0, y:0, scale:1. We need two var objects here
 // gsap.fromTo("img", { x:400, y:200, scale:3, opacity:0 }, { x:50, y:50, scale:1, duration:3, opacity:3 });
 
-// LESSON 3: Special Properties: Delay and Repeat
-// TO BE CONTINUED ...   
+// LESSON 3: Special Properties: Delay and Repeat, Yoyo, RepeatDelay, eases
+// gsap.to("img", { x:200, y:200, duration:3, rotation:360, ease:"bounce", delay:2, repeat:1 });
+// -1 means infinite repeat
+// gsap.to("img", { x:200, y:200, repeat:1, yoyo:true });
+// gsap.to("img", { x:200, y:200, duration:3, ease: "linear" });
+// gsap.to("img", { x:200, y:200, duration:3, ease: "elastic" });
+// gsap.to("img", { x:200, y:200, duration:3, ease: "bounce" });
+// gsap.to("img", { x:200, y:200, duration:3, ease: "bounce.in" });
+// gsap.to("img", { x:200, y:200, duration:3, ease: "bounce.out" }); // default
+// gsap.to("img", { x:200, y:200, duration:3, ease: "bounce.inOut" });
+// gsap.to("img", { x:200, y:200, duration:3, ease: "bounce.back" });
+// gsap.to("img", { x:200, duration:3, ease: "bounce.back(2)" }); // eases are functions, so we can pass arguments to them
+
+// ease curves: https://greensock.com/docs/v3/Eases (check the ease visualizer)
+
+// LESSON 4: Staggering
